799c9a3afa5ea854c18fc59890e22510335dcd4a
Diego Gimbernat <gimbernat13@gmail.com>
2019-11-28 17:05:05 -0600

    Nav deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
 import Users from "./Components/Users/Users";
-import Nav from "./Ui/Nav";
+
 // import Sidebar from "./Ui/Sidebar";
 
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      {/* <Sidebar /> */}
+    
       <div className="users-container">
         <Users />
       </div>
--- src/Ui/Nav.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Nav() {
-    return (
-        <div className="nav">
-            <h3>Jaque Test</h3>
-        </div>
-    )
-}
